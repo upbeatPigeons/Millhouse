b5713de683f0cfefa6b8d0ddb8d294539822dde8
Johanna <josjlin@gmail.com>
2018-11-21 15:23:47 +0100

    added comment about database_connection

--- includes/database_connection.php
+++ includes/database_connection.php
@@ -11,4 +11,6 @@ echo "Connected successfully \n";
 } catch (PDOException $e) {
 echo "Connection failed: " . $e->getMessage();
 }
+/* This is the connection we actually are going to use. The above is just for getting feedback during development, to see the database is actually working. */
+
 // $pdo = new PDO("mysql:host=$servername;dbname=fed18", $username, $password);
