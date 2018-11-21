a4b6f17084284af8308050592137886d4a844b67
viktorijavalsoe <viktorijavalsoe@gmail.com>
2018-11-21 15:49:22 +0100

    added head, navigation and home structure, and image folder

---
+++ includes/navigation.php
@@ -0,0 +1,29 @@
+<body>
+   
+    <div class="body-wrapper">
+
+        <header>
+            
+            <nav class="nav" role="navigation">
+              
+               <div class="logo">
+                  <a href="">
+                    <img src="" alt="Logo" class="logo-img">
+                 </a>
+               </div>
+               
+               <div>
+                
+                 <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                
+             </div>    
+
+            </nav>
+           
+        </header>
+    
---
+++ views/home.php
@@ -0,0 +1,71 @@
+<?php get_header(); ?>
+
+	<main>
+	    
+	    <section class="hero_image">
+           
+           <img>
+           
+        </section>
+	
+		<section class="latest_post">
+            <div>
+                <h1>Latest Posts</h1>
+            </div> 
+            
+            <div class="latest_post_container">
+                <div>
+                 <!--here we should fetch post title-->
+                    <h2></h2> 
+                </div>
+                
+                <div>
+                    <!--here we should fetch post date-->
+                    <h3></h3>
+                </div>   
+                
+                <div>
+                    <!--here we should fetch post content-->
+                    <p></p>
+                </div>  
+                
+                <div>
+                    <button></button>
+                </div>
+                
+                <div class="latest_post_image_container"> </div>            
+            </div>  
+            
+        </section>      
+
+			<aside>
+                <!--division-->
+                <div></div>
+			</aside>
+			
+		<section class="blog_posts">
+            <div class="single_blog_post">
+                <div>
+                    <!--here we should fetch post image-->
+                    <img>
+                </div>
+                
+                <div>
+                    <!--here we should fetch post title-->
+                    <h2></h2>
+                </div>
+                
+                <div>
+                    <!--read more button-->
+                    <button></button>
+                </div>
+                
+            </div>
+            
+        </section>	
+
+	</main>
+
+   
+
+<?php get_footer(); ?>
