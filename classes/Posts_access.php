c0c31269b69dfcfb5a11db607eb741a70cc66ff5
Ida Bennetsen <idaelisabethb@gmail.com>
2018-11-25 11:19:46 +0100

    Create Post Access object to add posts into database

---
+++ classes/Posts_access.php
@@ -0,0 +1,32 @@
+<?php 
+
+require_once "Post.php";
+
+class Access_posts
+{
+  private $pdo;
+  /* Inject the pdo connection so it is available inside of the class
+   * so we can call it with '$this->pdo', always available inside of the class
+   */
+  public function __construct($pdo)
+  {
+    $this->pdo = $pdo;
+  }
+
+   public function create(Post $new_post)
+  {    
+  	$statement = $this->pdo->prepare("INSERT INTO posts (title, description, created_by, date, category) VALUES (:title, :description, :created_by, :date, :category)");
+  	$statement->execute ([
+  		":title" => $new_post->get_title(),
+  		":description" => $new_post->get_description(),
+  		":created_by" => $new_post->get_created_by(),
+  		":date" => $new_post->get_date(),
+  		":category" => $new_post->get_category()
+  	]);
+  }
+
+}
+
+?>
+
+
