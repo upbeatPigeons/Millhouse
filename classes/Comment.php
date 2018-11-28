e702b71042c9cf54c352fb4e40daab76ebaa1755
viktorijavalsoe <viktorijavalsoe@gmail.com>
2018-11-28 21:26:17 +0100

    Add getter and setter to comment, prepare pdo statement comment method, delete create post redirect, create comment_controller

---
+++ classes/Comment.php
@@ -0,0 +1,69 @@
+Comment.php
+
+<?php
+
+/*
+* This class represents a comment.
+* It encapsulates its properties behind setter and getter methods
+* That means that the properties are private and cannot be modified after the object has been constructed
+* The only way to access the properties is through the getter methods 
+*/
+
+class Comment
+{
+    private $id;
+		private $content;
+	  private $postId;
+		private $createdBy;
+	  private $date;
+	
+	
+    public function __construct($content=NULL; $postId=NULL, $createdBy=NULL date=NULL){
+			
+			$this->content = $content;
+			$this->$postId =$postId;
+			$this->createdBy =createdBy;	
+			$this->date = $date("Y-m-d");
+    }
+	
+	
+	//setter functions 
+     public function set_content($content){
+			   $this->content = $content;	 
+		 }
+	  
+		public function set_postId($postId){
+			 	
+			  $this->postId = $postId;
+		}
+
+		public function set_createdBy($createdBy){
+			 	
+			  $this->createdBy = $createdBy;
+		}
+
+	
+	//getter functions
+	   public function get_content(){
+			 
+			 return $this->content;
+		 }
+	
+		 public function get_postId(){
+			 
+			 return $this->postId;
+		 }
+	
+	   public function get_createdBy(){
+			 
+			 return $this->createdBy;
+		 }
+	
+	   public function get_date(){
+			 
+			 return $this->$date;
+		 }
+	
+	 
+	
+}
---
+++ classes/CommentMethod.php
@@ -0,0 +1,31 @@
+<?php
+
+require "Comment.php";
+
+class CommentMethod{
+	
+	private $pdo;
+	
+	public function __construct($pdo){
+		
+	  $this->pdo =$pdo;
+	}
+	
+	public function create_comment(Comment $new_comment){
+		
+		$statement = $pdo->prepare("INSERT INTO comments(content, post_id, created_by, date) VALUES(:content, :post_id, :created_by, :date)");
+		
+		$statement->execute ([
+  		":content" => $new_comment->get_content(),
+  		":post_id" => $new_comment->get_postId(),
+      ":created_by" =>$new_comment->get_createdBy(),
+      ":date" => $new_post->get_date()
+    ]);
+  }
+		
+		
+	}
+}
+
+
+?>
