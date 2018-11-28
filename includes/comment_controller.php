a3341f02eb386fb12f4ad37d298394c9d3d69102
Johanna <josjlin@gmail.com>
2018-11-28 16:31:35 +0100

    no errors now, but not adding anything to database either

--- includes/comment_controller.php
+++ includes/comment_controller.php
@@ -1,7 +1,7 @@
 <?php
 require_once('database_connection.php');
 //require_once('../classes/CommentMethods.php');
-require_once('../classes/Comment.php');
+require_once('../classes/CommentMethods.php');
 
 $comment_content = $_POST["comment"];
 var_dump($comment_content);
@@ -9,5 +9,7 @@ var_dump($comment_content);
 
 $create_comment = new Comment($pdo);
 
-$new_comment = new CommentMethods();
+$new_comment = new CommentMethods($create_comment);
+
+//$create_comment->create_comment($new_comment);
 
