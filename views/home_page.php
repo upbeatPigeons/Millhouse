14fad5ae2e911e103ff1923effd03b828fc7e5b2
Johanna <josjlin@gmail.com>
2018-12-05 13:36:48 +0100

    Moved delete btn to single_post_page.php

--- views/home_page.php
+++ views/home_page.php
@@ -76,10 +76,6 @@
 				<div>
 					<a href="single_post_page.php?id=<?= $post->get_id();?>" class="btn btn-outline-primary">Read more</a>
 				</div>
-				
-				<form action="../includes/actions_posts.php?id=<?= $post->get_id();?>" method="POST">
-				<input type="submit" name="remove_post" value="Delete Post" class="btn btn-outline-primary" />
-				</form>
 				<!-- Col -->	
 			</div><?php }?>
 		</section>			
