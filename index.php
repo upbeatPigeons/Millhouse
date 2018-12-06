0e70c57f2f3525fbb05efe79cff99aeab71d521d
Johanna <josjlin@gmail.com>
2018-12-06 15:26:53 +0100

    top of page in mobile looks fine, but have to check breakpoints

--- index.php
+++ index.php
@@ -2,35 +2,31 @@
     require "includes/head.php"
 ?>
 
-
-
 <body id="index_page">
 
   <header class="row d-block d-md-none index_header">
-    <!--Background Image on smaller screens-->
-    <div class="hero_image" style="background-image: url('images/Eske_01.jpg');">
-    </div>
-
-    
-    <nav class="navbar fixed-top scrolling-navbar">
-      <div class="container logo_container">
-        <!--Navbar Logo-->
-        <img class="logo-image" src="images/logo_dark.png" alt="Logo">
-      </div>
-    </nav>
+    <div class="header_container"></div>
   </header>
-      
+  
 
   <main class="index_container">
- 
-    <section class="container index_desktop">
+    
+    <!-- Background-image for smaller screens -->
+    <div class="d-block d-md-none background_image">
+      <div class="container logo_container">
+      <!--Smallscreens Logo-->
+      <img class="logo-image" src="images/logo_dark.png" alt="Logo">
+    </div>
+    </div>
 
-      <div class="row justify-content-around">
-        <!--Image with interior for bigger screens-->
-        <div class="d-none d-md-block col-md-5 index_image">
-          <img class="w-100" src="images/Eske_01.jpg" alt="Millhouse Store">
-        </div>
+    <div class="row justify-content-around">
+      <!--Image with interior for bigger screens-->
+      <div class="d-none d-md-block col-md-5 index_image">
+        <img class="w-100" src="images/Eske_01.jpg" alt="Millhouse Store">
       </div>
+    </div>
+
+    <section class="container">
 
       <div class="d-none d-md-block col-md-5 index_content">
         <!--Logo for bigger screens-->
@@ -62,7 +58,6 @@
   ?>
 
 </body>
-
 
 </html>
 
