ea2d7b1a07a67e449c21d7d66fadcda022cbce42
Johanna <josjlin@gmail.com>
2018-12-07 15:04:19 +0100

    renaming classnames

--- index.php
+++ index.php
@@ -5,21 +5,21 @@ require "includes/head.php"
 <body id="index_page">
 
   <header class="row d-block d-md-none index_header">
-    <div class="header_container"></div>
+    <div class="small_screen_header_container"></div>
   </header>
   
     <!-- Background-image for smaller screens -->
-    <div class="d-block d-md-none background_image">
+    <div class="d-block d-md-none small_screen_background_image">
 
       <!--Smallscreens Logo-->
       <div class="container logo_container">
-        <img class="logo-image" src="images/logo_dark.png" alt="Logo">
+        <img class="small_screen_logo_image" src="images/logo_dark.png" alt="Logo">
       </div>
 
       <!-- Container for elements we want to flex-end -->
       <div class="flex_bottom_container">
         <!-- Container for buttons on small screen  -->
-        <div class="index_button_container justify-content-center">
+        <div class="small_screen_button_container justify-content-center">
          
           <button type="button" onclick="location.href='views/login_page.php';" class="btn    btn-outline-primary ">
             Log in
@@ -34,12 +34,12 @@ require "includes/head.php"
 
     </div> <!-- Background-image for smaller screens -->
 
-    <div class="cube_color d-md-none">
+    <div class="small_screen_cube_color d-md-none">
       <div class="hero">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat magni necessitatibus at optio, quod adipisci modi veritatis impedit vero. Reprehenderit? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, aperiam.
 
       </div>
-      <div class="hr_imitaion"></div>
+      <div class="hr_imitation"></div>
 
       <?php
       require "includes/footer.php"
@@ -53,21 +53,21 @@ require "includes/head.php"
       <div class="row justify-content-around">
 
         <!-- Container for image on bigger screens -->
-        <div class="d-none d-md-flex col-md-8 col-lg-5 justify-content-center index_image">
+        <div class="d-none d-md-flex col-md-8 col-lg-5 justify-content-center big_screen_image">
             <img class="img-fluid" src="images/Eske_01.jpg" alt="Millhouse Store Responsive image">
         </div>
 
         <!-- Wrapper for logo and buttons on bigger screens -->
-        <div class="d-none d-md-flex col-md-4 col-lg-5 index_logo_btn_container">
+        <div class="d-none d-md-flex col-md-4 col-lg-5 big_screen_logo_btn_container">
 
           <div>
             <!--Logo for bigger screens-->
-            <div class="index_logo">
+            <div class="big_screen_logo">
               <img class="img-fluid" src="images/logo_dark.png" alt="Millhouse Logo">
             </div> <!--Logo for bigger screens-->
 
             <!-- Container for buttons  -->
-            <div class="index_button_container">
+            <div class="big_screen_button_container">
             
               <button type="button" onclick="location.href='views/login_page.php';" class="btn    btn-outline-primary ">
                 Log in
