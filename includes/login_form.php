c15649b2e0e4479b7d335c53618d0820b528a713
Lukas Lennstring <lulapwnie@gmail.com>
2018-12-07 14:53:38 +0100

    Removed duplicate error

--- includes/login_form.php
+++ includes/login_form.php
@@ -4,13 +4,12 @@
   <div class="form-group">
      <label for="username">Enter your username</label>
      <input type="text" class="form-control" name="username" id="username" placeholder="Username" required>
-     <!-- Gets error and formats it -->
-     <?php show_error_messages($_GET["error"]); ?> 
   </div>         
 
   <div class="form-group">
      <label for="password">Enter your password</label>
      <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+     <!-- Gets error and formats it -->
      <?php show_error_messages($_GET["error"]); ?> 
   </div>
   <button type="submit" name="login" value="Log in" class="btn btn-outline-primary btn-m btn-block">Sign in</button>
