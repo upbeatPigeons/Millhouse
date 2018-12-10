efe97b43407fc8cd640e7b2865b6797c2356cb1e
Lukas Lennstring <lulapwnie@gmail.com>
2018-12-10 21:40:44 +0100

    Styling test and reindentation

--- includes/nav-bar.php
+++ includes/nav-bar.php
@@ -1,50 +1,59 @@
 <nav class="navbar-logo">
-       <div class="row justify-content-center">
-            <a class="navbar-brand" href="../views/home_page.php">
-                <img class="img-fluid" src="../images/blue_logo%20_small.png" alt="Logo">
-            </a>
-       </div>
-   </nav>
+  <div class="row justify-content-center">
+    <a class="navbar-brand" href="../views/home_page.php">
+      <img class="img-fluid" src="../images/blue_logo%20_small.png" alt="Logo">
+    </a>
+  </div>
+</nav>
 
-   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-   
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-           
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-               
-                <ul class="navbar-nav navbar-center">
-                    <li class="nav-item">
-                        <a class="nav-link" href="../views/home_page.php">Blog</a>
-                    </li>
-                    <?php if (isset($_SESSION["username"])) {?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../includes/logout.php">Log out</a>
-                    </li>
-                    <?php } else {?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../views/login_page.php">Sign in</a>
-                    </li>
-                    <?php }?>
-                    <!-- Check if the user is an admin-->
-                    <?php if ($_SESSION["admin"] == 1){?> 
-                    <li class="nav-item">
-                        <a class="nav-link" href="../views/create_post_page.php">Create Post</a>
-                    </li>
-                    <?php }?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../views/about_page.php">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../views/contact_page.php">Contact</a>
-                    </li>
-                </ul>
-                
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+        
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        
+    <ul class="navbar-nav navbar-center">
+
+      <li class="nav-item">
+        <a class="nav-link" href="../views/home_page.php">Blog</a>
+      </li>
+
+      <?php if (isset($_SESSION["username"])) { ?>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="../includes/logout.php">Log out</a>
+        </li>
+
+      <?php } else {?>
+
+        <li class="nav-item">
+          <a class="nav-link" href="../views/login_page.php">Sign in</a>
+        </li>
+
+      <?php }?>
+
+
+      <!-- Check if the user is an admin-->
+      <?php if ($_SESSION["admin"] == 1){?> 
+
+        <li class="nav-item">
+          <a class="nav-link" href="../views/create_post_page.php">Create Post</a>
+        </li>
+
+      <?php }?>
+
+      <li class="nav-item">
+        <a class="nav-link" href="../views/about_page.php">About</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="../views/contact_page.php">Contact</a>
+      </li>
+
+    </ul>
+    
+  </div>
+</nav>
         
