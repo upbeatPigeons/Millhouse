38813fe86504d70c0543d19177ef734d23aa0799
Johanna <josjlin@gmail.com>
2018-12-10 16:56:18 +0100

    moved css link so it's after bootstrap

--- includes/head.php
+++ includes/head.php
@@ -7,6 +7,12 @@
     
     <title>Millhouse</title>
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    
+    <link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans:400,400i,700" rel="stylesheet">
+
     <!--checks if file location is within 
     views folder using getcwd which returns path-->
     <?php $path = getcwd();
@@ -15,13 +21,6 @@
     <?php var_dump($path); } else { ?>
         <link rel="stylesheet" href="../css/style.css">    
     <?php } ?>
-
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    
-    <link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans:400,400i,700" rel="stylesheet">
 
 </head>
 
