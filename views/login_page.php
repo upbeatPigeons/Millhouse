ae7eb568ab56708c991b6dfbcf0cdd1e0d974cbc
viktorijavalsoe <viktorijavalsoe@gmail.com>
2018-12-10 13:00:11 +0100

    added if function to login page to show only login or register form

--- views/login_page.php
+++ views/login_page.php
@@ -10,43 +10,48 @@ require "../includes/validation_functions.php";
 <body id="login_page">
 	<header>
 		<nav class="navbar-logo">
-     <div class="row justify-content-center">
+			<div class="row justify-content-center">
 
-      <a class="navbar-brand" href="../index.php">
+				<a class="navbar-brand" href="../index.php">
 
-        <img class="img-fluid" src="../images/blue_logo%20_small.png" alt="Logo">
-      </a>
-    </div>
-  </nav>
+					<img class="img-fluid" src="../images/blue_logo%20_small.png" alt="Logo">
+				</a>
+			</div>
+		</nav>
 
-  <?php 
+		<?php 
   include "../includes/carousel.php";
   ?>
-</header>
+	</header>
 
-<main>
-  <section class="container login-container">
-   <h1>Log in</h1>
-   <hr>
+	<main>
+		<section class="container login-container">
+			<h1>Log in</h1>
+			<hr>
 
-   <div class="row signin-wrapper justify-content-between">
-    <div class="col-sm-12 col-md-6">
-      <!-- Include the login form-->
-      <?php include "../includes/login_form.php"; ?>
-      <!-- Include the register form-->
-      <?php include "../includes/register_form.php"; ?>
-    </div> <!-- End of col -->
+			<div class="row signin-wrapper justify-content-between">
+				<div class="col-sm-12 col-md-6">
 
-    <div class="d-none d-md-block col-md-6">
-     <img class="login_side_image w-100" src="../images/details.jpg" alt="Scented candle">
-   </div> <!-- end of col -->
-</div> <!-- End of row -->
+					<?php if ($_GET['action'] == "login"){
+          /* Include the login form */
+          	include "../includes/login_form.php"; 
+     
+            }else{
+          /* Include the register form */
+    					include "../includes/register_form.php";
+            }?>
+				</div> <!-- End of col -->
 
-</section>
+				<div class="d-none d-md-block col-md-6">
+					<img class="login_side_image w-100" src="../images/details.jpg" alt="Scented candle">
+				</div> <!-- end of col -->
+			</div> <!-- End of row -->
 
-</main>
+		</section>
 
-<?php
+	</main>
+
+	<?php
 require "../includes/footer.php"
 ?>
 </body>
