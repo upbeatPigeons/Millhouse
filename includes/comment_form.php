67a79ea3ae8cba0765f7b9a53cbacbbfbaa63a4a
viktorijavalsoe <viktorijavalsoe@gmail.com>
2018-12-11 20:09:32 +0100

    final design for single post page and remove error message comment

--- includes/comment_form.php
+++ includes/comment_form.php
@@ -5,14 +5,13 @@
  <?php
 require_once "validation_functions.php";
 ?>
-<div class="container comment_field">
 	<form action="../includes/comment_controller.php?action=create&id=<?php echo $post->get_id();?>" method="POST">
-		<div class="row form-group">
+		<div class="row form-group comment_form justify-content-center">
+			<div class="col-12 col-md-9">
+				<h3 class="smaller_title">Leave a comment</h3>
+			</div>
 			<label for="comment" class="sr-only">Leave a comment</label>
-			<textarea type="text-area" class="col-12 text-area" name="comment" placeholder="Leave your Comment"></textarea>
-			 <!-- Gets error and formats it -->
-			<div><?php show_error_messages($_GET["error"]); ?></div>
-	  	<button class="btn comment_button col-12" type="submit">Submit</button>	
+			<textarea type="text-area" class="col-12 col-md-9 text-area" name="comment" placeholder="Leave your Comment" required></textarea>
+	  	<button class="btn comment_button col-12 col-md-9" type="submit">Submit</button>	
 		</div>
 	</form>	 
-</div>
