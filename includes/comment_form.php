f1c195f0f526d228b5ec22892567af3d2cec3fdf
viktorijavalsoe <viktorijavalsoe@gmail.com>
2018-12-11 15:05:53 +0100

    updte comment form

--- includes/comment_form.php
+++ includes/comment_form.php
@@ -2,18 +2,17 @@
 <!--Sends post id as GET in url with $post->get_id()
     and action create to initiate switch function in comment controller
  -->
-<?php
+ <?php
 require_once "validation_functions.php";
 ?>
-          
-<form action="../includes/comment_controller.php?action=create&id=<?php echo $post->get_id();?>" method="POST">
-	<div class="form-group">
-		<label for="comment">Leate a comment</label>
-		<textarea type="text-area" class="text-area" name="comment" placeholder="Leave your Comment"></textarea>
-		 <!-- Gets error and formats it -->
-		<div><?php show_error_messages($_GET["error"]); ?></div>
-	  <button type="submit">Submit</button>	
-	</div>
-</form>	 
-
-	
+<div class="container comment_field">
+	<form action="../includes/comment_controller.php?action=create&id=<?php echo $post->get_id();?>" method="POST">
+		<div class="row form-group">
+			<label for="comment" class="sr-only">Leave a comment</label>
+			<textarea type="text-area" class="col-12 text-area" name="comment" placeholder="Leave your Comment"></textarea>
+			 <!-- Gets error and formats it -->
+			<div><?php show_error_messages($_GET["error"]); ?></div>
+	  	<button class="btn comment_button col-12" type="submit">Submit</button>	
+		</div>
+	</form>	 
+</div>
