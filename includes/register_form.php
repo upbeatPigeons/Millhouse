63dc6fc3d8ec252b32603982acdd6541d5d0af66
Johanna <josjlin@gmail.com>
2018-12-11 18:36:12 +0100

    added button styles to register page

--- includes/register_form.php
+++ includes/register_form.php
@@ -18,6 +18,6 @@
   <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="Confirm password" required>
   <?php show_error_messages($_GET["confirm_password_error"]); ?>  
 </div>
-<button type="submit" name="register" class="btn btn-outline-primary btn-m btn-block" value="Register">Register</button>
+<button type="submit" name="register" class="btn primory_btn btn-m btn-block" value="Register">Register</button>
 </form>
 <?php show_error_messages($_GET["user_registered"]); ?> 
