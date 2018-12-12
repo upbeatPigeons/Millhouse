0c940abb31875e9e9f8625afba406089505b3d65
viktorijavalsoe <viktorijavalsoe@gmail.com>
2018-12-12 21:23:03 +0100

    updated images, added caption

--- includes/carousel.php
+++ includes/carousel.php
@@ -1,20 +1,39 @@
-<div id="carousel_indicators" class="carousel slide" data-ride="carousel">
+
+<!-- Indicators -->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
-    <li data-target="#carousel_indicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel_indicators" data-slide-to="1"></li>
-    <li data-target="#carousel_indicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
+
+    <!-- The slideshow-->
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="../images/kitchen.jpg" alt="First slide">
+      <img class="d-block w-100" src="../images/kitchen.jpg" alt="Kitchen">
+      <div class="carousel-caption d-none d-md-block">
+        <h3 class="carousel_title">Millenium pink is here to stay</h3>
+        <p class="carousel_content">Find back to the chind in you with playfull shades of pink</p>
+      </div>
     </div>
+
     <div class="carousel-item">
-      <img class="d-block w-100" src="../images/living_room.jpg" alt="Second slide">
-    </div>
+      <img class="d-block w-100" src="../images/living_room1.png" alt="Living room">
+      <div class="carousel-caption d-none d-md-block">
+        <h3 class="carousel_title">Millenium pink is here to stay</h3>
+        <p class="carousel_content">Find back to the chind in you with playfull shades of pink</p>
+      </div>
+     </div>
+
     <div class="carousel-item">
-      <img class="d-block w-100" src="../images/pink_sofa.jpg" alt="Third slide">
+      <img class="d-block w-100 carousel_image" src="../images/pink_sofa1.jpg" alt="Pink sofa">
+      <div class="carousel-caption d-none d-md-block">
+        <h3 class="carousel_title">Millenium pink is here to stay</h3>
+        <p class="carousel_content">Find back to the chind in you with playfull shades of pink</p>
+      </div>
     </div>
-  </div>
+
+  <!-- Left and right controls-->
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
@@ -23,4 +42,4 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
-</div>   
+</div>
