fcb1596c073264e0020bd345bc64a23b6143bb4c
Ida Bennetsen <idaelisabethb@gmail.com>
2018-12-12 13:57:24 +0100

    Style the create post page, edit post page and their forms

--- views/create_post_page.php
+++ views/create_post_page.php
@@ -1,22 +1,26 @@
 <?php 
 session_start();
 include "../includes/head.php";
+include "../includes/actions_posts.php";
 ?>
 
-	<body id="create_post_page">
-		<header>
-			<?php include "../includes/nav-bar.php"; ?>
-		</header>
-    
-		<main>
-			<?php 
-				include "../includes/actions_posts.php";
-        include "../includes/create_post_form.php"; 
-			?>
-		</main>
-		
-		<?php include "../includes/footer.php"; ?>
-	</body>
+<body id="create_post_page">
+	<header>
+		<?php include "../includes/nav-bar.php"; ?>
+	</header>
+	
+	<main class="main_content">
+		<section class="container form_container">
+			<div class="form_wrapper">
+				<?php 
+				include "../includes/create_post_form.php"; 
+				?>
+			</div>
+		</section>
+	</main>
+	
+	<?php include "../includes/footer.php"; ?>
+</body>
 </html>
 
 
