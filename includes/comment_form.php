294c618b9966b2f10ea3fe0873714e5a07e186f3
viktorijavalsoe <viktorijavalsoe@gmail.com>
2018-12-12 15:16:13 +0100

    adjested margins and padding in comment form

--- includes/comment_form.php
+++ includes/comment_form.php
@@ -7,11 +7,11 @@ require_once "validation_functions.php";
 ?>
 	<form action="../includes/comment_controller.php?action=create&id=<?php echo $post->get_id();?>" method="POST">
 		<div class="row form-group comment_form justify-content-center">
-			<div class="col-12 col-md-9">
+			<div class="col-12 col-md-9 comment_field_title">
 				<h3 class="smaller_title">Leave a comment</h3>
 			</div>
 			<label for="comment" class="sr-only">Leave a comment</label>
-			<textarea type="text-area" class="col-12 col-md-9 text-area" name="comment" placeholder="Leave your Comment" required></textarea>
+			<textarea type="text-area" class="col-12 col-md-9 text-area" name="comment" required></textarea>
 	  	<button class="btn comment_button primory_btn col-12 col-md-9" type="submit">Submit</button>	
 		</div>
 	</form>	 
