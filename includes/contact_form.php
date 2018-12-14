debe3c2d1da034512f998a1b3534d0691dbc8864
Ida Bennetsen <idaelisabethb@gmail.com>
2018-12-14 12:48:56 +0100

    Style contact form

--- includes/contact_form.php
+++ includes/contact_form.php
@@ -1,24 +1,24 @@
-
-<h3 class="smaller_title">Leave a comment</h3>
    <div class="container">
-      <form class="login" action="/action_page.php">
+      <h3 class="smaller_title">Contact us</h3>
+      <form class="login" action="mail.php" method="POST">
          <div class="form-group">
-            <label for="fname">First Name</label>
-            <input type="text" class="form-control borders" id="fname" name="firstname" placeholder="Your name..">
-         <div>
+            <label for="fname">First name</label>
+            <input type="text" class="form-control borders" id="fname" name="firstname" placeholder="Your name">
+         </div>
     
       <div class="form-group">
-         <label for="lname">Email</label>
-         <input type="text" class="form-control borders" id="lname" name="lastname" placeholder="Your email.." required>
+         <label for="email">Email address</label>
+         <input type="text" class="form-control borders" id="email" name="lastname" placeholder="Your email address" required>
       </div>
 
       <div class="form-group">
-		   <label for="comment" class="sr-only">Leave a comment</label>
-         <textarea type="text-area" class="col-12 col-md-9 text-area" name="comment" required></textarea>
-      </div>   
-	  	<button class="btn comment_button primory_btn col-12 col-md-9" type="submit">SUBMIT</button>	
+		   <label for="message">Write a message</label>
+         <textarea class="form-control borders" name="message" rows="5"cols="30" placeholder="Write something to us" required></textarea>
+      </div>  
+      <div class="cta_area"> 
+	  	<button class="btn contact_button primory_btn btn-block" type="submit" value="send">SEND</button>
+      <input class="btn contact_button primory_btn btn-block" type="reset" value="CLEAR">
+   </div>
 	</form>
-
- <!-- Container --> 
-</div>
+</div> <!-- end of container --> 
 
