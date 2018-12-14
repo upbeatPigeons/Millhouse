1f358d089060bda0b48fc693ba6187bd8390b23b
Ida Bennetsen <idaelisabethb@gmail.com>
2018-12-14 17:01:56 +0100

    Send emails from the form and show alerts ifit was successful or not

--- includes/mail.php
+++ includes/mail.php
@@ -5,19 +5,25 @@ if ($_GET["action"] === "send") {
   $email = htmlspecialchars(stripslashes(trim($_POST["email"])));
   $subject = htmlspecialchars(stripslashes(trim($_POST["subject"])));
   $message = htmlspecialchars(stripslashes(trim($_POST["message"])));
-  $website = $_POST["website"];
 
   // Validations below
 
+  // Name validation: Cannot be empty and cannot contain invalid characters
   if(!preg_match("/^[A-Za-z .'-]+$/", $name)) {
     $name_error = "Invalid name";
   }
-  if(!preg_match("/^[A-Za-z .'-]+$/", $subject)) {
+
+  // Subject validation: Cannot be empty and cannot contain invalid characters
+  if(!preg_match("/^[A-Za-z0-9 .'-\?]+$/", $subject)) {
     $subject_error = "Invalid subject";
   }
+
+  // Email validation: Cannot be empty and cannot contain invalid characters
   if(!preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/", $email)) {
     $email_error = "Invalid email";
   }
+
+  // Message validation: Cannot be empty
   if(strlen($message) === 0){
     $message_error = "Your message cannot be empty";
   }
@@ -28,11 +34,11 @@ if ($_GET["action"] === "send") {
     $body="From: $name \n Message: $message";
   // Change recipient to company email address later
     $recipient = "idaelisabethb@gmail.com";
-  //$mailheader = "From: $email \r\n";
+
     if (mail($recipient, $subject, $body)) {
-      header("Location: ../views/about_page.php?emailSent=true");
+      header("Location: ../views/about_page.php?email_sent=true");
     } else {
-      header("Location: ../views/about_page.php?emailSent=false");
+      header("Location: ../views/about_page.php?email_sent=false");
     }
   }
 }
--- views/about_page.php
+++ views/about_page.php
@@ -6,28 +6,10 @@
 ?>
 
 <body id="about_page">
-	<?php if (isset($_GET["emailSent"])) {
-		/* This alert will show if the email is sent successfully
-		* Made with https://getbootstrap.com/docs/4.0/components/alerts/
-		* and https://getbootstrap.com/docs/4.0/components/alerts/#dismissing
-		*/
-		if ($_GET["emailSent"] == "true") { ?>
-			<div class="alert alert-success alert-dismissible fade show content" role="alert">
-			  <strong>Email sent!</strong> Thank you for your message.
-			  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			    <span aria-hidden="true">&times;</span>
-			  </button>
-			</div>
-		<?php } else { 
-			// This alert will show if there was a problem with sending the email ?>
-			<div class="alert alert-danger alert-dismissible fade show content" role="alert">
-			  <strong>There was a problem sending your email.</strong> Please wait a few minutes and try again.
-			  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			    <span aria-hidden="true">&times;</span>
-			  </button>
-			</div>
-		<?php }
-	}?>
+
+	<?php 
+	include "../includes/alerts.php";
+	?>
 
 	<header>
 		<?php
