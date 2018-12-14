2aa257453abef065c304936558df3e1ca04841f6
Ida Bennetsen <idaelisabethb@gmail.com>
2018-12-14 16:09:22 +0100

    WIP: Prevent email from ending up in spam folder

--- includes/contact_form.php
+++ includes/contact_form.php
@@ -1,29 +1,35 @@
+<?php 
+
+require_once "validation_functions.php";
+
+?>
+
    <div class="container">
       <h3 class="smaller_title">Contact us</h3>
-      <form class="login" action="../includes/mail.php" method="POST">
+      <form class="login" action="about_page.php?action=send" method="POST">
          <div class="form-group">
             <label for="name">Name</label>
             <input type="text" class="form-control borders" id="name" name="name" placeholder="Your name">
-            <p><?php if(isset($name_error)) echo $name_error; ?></p>
+            <div class="<?php get_class_for_error_message($name_error)?>"><?php echo $name_error;?></div>
          </div>
     
       <div class="form-group">
          <label for="email">Email address</label>
          <input type="text" class="form-control borders" id="email" name="email" placeholder="Your email address">
-         <p><?php if(isset($email_error)) echo $email_error; ?></p>
+         <div class="<?php get_class_for_error_message($email_error)?>"><?php echo $email_error;?></div>
       </div>
 
       <div class="form-group">
          <label for="subject">Subject</label>
          <input type="text" class="form-control borders" id="subject" name="subject" placeholder="Subject">
-         <p><?php if(isset($subject_error)) echo $subject_error; ?></p>
+         <div class="<?php get_class_for_error_message($subject_error)?>"><?php echo $subject_error;?></div>
       </div>
 
       <div class="form-group">
 		   <label for="message">Write a message</label>
          <textarea class="form-control borders" name="message" rows="5"cols="30" placeholder="Write something to us"></textarea>
-         <p><?php if(isset($message_error)) echo $message_error; ?></p>
-      </div>  
+         <div class="<?php get_class_for_error_message($message_error)?>"><?php echo $message_error;?></div>
+      </div> 
       <div class="cta_area"> 
 	  	<input type="submit" class="btn contact_button primory_btn btn-block" value="SUBMIT">
       <input class="btn contact_button primory_btn btn-block" type="reset" value="CLEAR">
--- includes/mail.php
+++ includes/mail.php
@@ -1,34 +1,39 @@
 <?php 
-if (isset($_POST["submit"])) {
+if ($_GET["action"] === "send") {
 
   $name = htmlspecialchars(stripslashes(trim($_POST["name"])));
   $email = htmlspecialchars(stripslashes(trim($_POST["email"])));
   $subject = htmlspecialchars(stripslashes(trim($_POST["subject"])));
   $message = htmlspecialchars(stripslashes(trim($_POST["message"])));
+  $website = $_POST["website"];
+
+  // Validations below
 
   if(!preg_match("/^[A-Za-z .'-]+$/", $name)) {
-    $name_error = 'Invalid name';
+    $name_error = "Invalid name";
   }
   if(!preg_match("/^[A-Za-z .'-]+$/", $subject)) {
-    $subject_error = 'Invalid subject';
+    $subject_error = "Invalid subject";
   }
   if(!preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/", $email)) {
-    $email_error = 'Invalid email';
+    $email_error = "Invalid email";
   }
   if(strlen($message) === 0){
-    $message_error = 'Your message should not be empty';
+    $message_error = "Your message cannot be empty";
   }
-}
 
-if(isset($_POST["submit"]) && !isset($name_error) && !isset($subject_error) && !isset($email_error) && !isset($message_error)) {
-  $formcontent="From: $name \n Message: $message";
+  // If there are no errors, send the email
+
+  if(!isset($name_error) && !isset($subject_error) && !isset($email_error) && !isset($message_error)) {
+    $body="From: $name \n Message: $message";
   // Change recipient to company email address later
-  $recipient = "idaelisabethb@gmail.com";
+    $recipient = "idaelisabethb@gmail.com";
   //$mailheader = "From: $email \r\n";
-  if (mail($recipient, $subject, $formcontent)) {
-    header("Location: ../views/about_page.php");
-  } else {
-    header("Location: ../views/about_page.php");
+    if (mail($recipient, $subject, $body)) {
+      header("Location: ../views/about_page.php?emailSent=true");
+    } else {
+      header("Location: ../views/about_page.php?emailSent=false");
+    }
   }
 }
 
