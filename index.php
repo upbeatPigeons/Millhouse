79ce561441545f1bb7fea459714a31fa6870c068
viktorijavalsoe <viktorijavalsoe@gmail.com>
2018-12-16 19:12:47 +0100

    changed to include loginform index

--- index.php
+++ index.php
@@ -18,25 +18,7 @@ require "includes/validation_functions.php";
         <!-- Container for buttons  -->
 
         <div class="small_screen_button_container justify-content-between">
-
-          <form class="login" action="../includes/login_controller.php" method="POST">
-            <div class="form-group">
-              <label for="username" class="sr-only">Enter your username</label>
-              <input type="text" class="form-control borders" name="username" id="username" placeholder="Username" required>
-            </div>         
-
-            <div class="form-group">
-              <label for="password" class="sr-only">Enter your password</label>
-              <input type="password" class="form-control borders" name="password" id="password" placeholder="Password" required>
-              <!-- Gets error and formats it -->
-              <?php show_error_messages($_GET["error"]); ?> 
-            </div>
-            <button type="submit" name="login" value="Log in" class="primory_btn_inverse btn btn-m btn-block">SIGN IN</button>
-          </form>
-
-          <div class="login_text_box content">
-            <p class="body1">Don't have an account?<a class="body2" href="views/login_page.php?action=register"> Sign up here</p></a></div> 
-          </div>
+          <?php include "includes/login_form.php"; ?>
          
         </div> <!-- Container for buttons  -->
 
@@ -74,26 +56,8 @@ require "includes/validation_functions.php";
               <div class="big_screen_button_container">
 
                 <div class="button_container">   
-                  <form class="login" action="../includes/login_controller.php" method="POST">
-                    <div class="form-group">
-                      <label for="username" class="sr-only">Enter your username</label>
-                      <input type="text" class="form-control borders" name="username" id="username" placeholder="Username" required>
-                    </div>         
-
-                    <div class="form-group">
-                      <label for="password" class="sr-only">Enter your password</label>
-                      <input type="password" class="form-control borders" name="password" id="password" placeholder="Password" required>
-                      <!-- Gets error and formats it -->
-                      <?php show_error_messages($_GET["error"]); ?> 
-                    </div>
-                  <button type="submit" name="login" value="Log in" class="primory_btn_inverse btn btn-m btn-block">LOG IN</button>
-                </form>
-
-                <div class="login_text_box text-center">
-                  <p class="body1">Don't have an account?<a class="body2" href="views/login_page.php?action=register"> Sign up here</p></a></div>          
-                </div>  
-
-              </div> <!-- Container for buttons  -->
+                  <?php include "includes/login_form.php"; ?> 
+                </div> <!-- Container for buttons  -->
             </div> 
 
           </div> <!-- Wrapper for logo and buttons on bigger screens -->
