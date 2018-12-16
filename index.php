e334ac057fa755402d82316c99c090120cf5dd65
viktorijavalsoe <viktorijavalsoe@gmail.com>
2018-12-16 18:27:48 +0100

    change margins index tablet

--- index.php
+++ index.php
@@ -71,24 +71,23 @@ require "includes/validation_functions.php";
               <!-- Container for buttons  -->
               <div class="big_screen_button_container">
 
-             <div class="button_container">   
+                <div class="button_container">   
+                  <form class="login" action="../includes/login_controller.php" method="POST">
+                    <div class="form-group">
+                      <label for="username" class="sr-only">Enter your username</label>
+                      <input type="text" class="form-control borders" name="username" id="username" placeholder="Username" required>
+                    </div>         
 
-              <form class="login" action="../includes/login_controller.php" method="POST">
-            <div class="form-group">
-              <label for="username" class="sr-only">Enter your username</label>
-              <input type="text" class="form-control borders" name="username" id="username" placeholder="Username" required>
-            </div>         
+                    <div class="form-group">
+                      <label for="password" class="sr-only">Enter your password</label>
+                      <input type="password" class="form-control borders" name="password" id="password" placeholder="Password" required>
+                      <!-- Gets error and formats it -->
+                      <?php show_error_messages($_GET["error"]); ?> 
+                    </div>
+                  <button type="submit" name="login" value="Log in" class="primory_btn_inverse btn btn-m btn-block">LOG IN</button>
+                </form>
 
-            <div class="form-group">
-              <label for="password" class="sr-only">Enter your password</label>
-              <input type="password" class="form-control borders" name="password" id="password" placeholder="Password" required>
-              <!-- Gets error and formats it -->
-              <?php show_error_messages($_GET["error"]); ?> 
-            </div>
-            <button type="submit" name="login" value="Log in" class="primory_btn_inverse btn btn-m btn-block">SIGN IN</button>
-          </form>
-
-          <div>Don't have an account?<a href="views/login_page.php?action=register"> Sign up here</a></div>          
+          <div class="login_text_box content">Don't have an account?<a href="views/login_page.php?action=register"> Sign up here</a></div>          
                 
                 </div>  
 
