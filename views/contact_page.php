cea6d481f97ea4eb05062f8950e935aa78360973
Lukas Lennstring <lulapwnie@gmail.com>
2018-12-18 16:04:13 +0100

    Contact page indentation

--- views/contact_page.php
+++ views/contact_page.php
@@ -1,16 +1,17 @@
 <?php
-    session_start();
-    include "../includes/head.php"; 
-    include "../includes/force_login.php"; 
-?>
 
+session_start();
+include "../includes/head.php"; 
+include "../includes/force_login.php"; 
+
+?>
 
 <body id="contact_page">
 
 	<header>
 		<?php
-    	include "../includes/nav-bar.php";
-			include "../includes/carousel.php";
+		include "../includes/nav-bar.php";
+		include "../includes/carousel.php";
 		?>
 	</header>
 	
@@ -18,7 +19,6 @@
 		<section class="container login_container">
 			<h1>Get in Touch</h1>
 			<hr>
-			
 			<div class="row">
 				<div class="col-12 col-md-6">
 					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
@@ -30,10 +30,10 @@
 				</div>
 			<!-- Row -->
 			</div>
-	
-<?php
-    require "../includes/footer.php"
-    ?>
+
+	<?php
+	require "../includes/footer.php"
+	?>
 </body>
 
 </html>
